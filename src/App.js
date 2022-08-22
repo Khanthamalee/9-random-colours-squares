7ceb38ab9809a55cea285b0791ce337cc0093eda
Malee <n.Khanthamalee@gmail.com>
2022-08-22 15:10:35 +0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import './App.css';
+import './colours';
+
+
+function App() {
+
+  return (
+      <header className="App">
+        <h2><center>
+          Name : Khanthamalee Na-udom
+        </center></h2>
+        <a href="http://localhost:3000/">
+          <button class="bubbly-button" >Random-colours</button>
+        </a>
+      </header>
+  );
+}
+export default App;
---
+++ src/colours.js
@@ -0,0 +1,34 @@
+
+//https://lnwquiz.com/post/1083/เขียนโค้ดสุ่มตัวเลขและตัวอักษรไม่ซ้ำด้วยภาษา-JavaScript?fbclid=IwAR21aBjcUH8jLsPVbB2l6YxGd1ToxUu89pDfrDx-vHwzppDIHwgHUNtsPRk
+function Randomcolours(length) {
+    let characters ='ABCDEF0123456789';
+    let result = '';
+    //นับจำนวนตัวอักษร
+    let charactersLength = characters.length;
+    //วนลูปตามจำนวนตัวอักษร
+    for ( let i = 0; i < length; i++ ) {
+        //เอาตัวอักษรตามตำแหน่งที่ random มาได้เก็บไว้ในตัวแปร result
+        result += characters.charAt(Math.floor(Math.random() *charactersLength));
+    }
+
+    return result;
+}
+    
+function squares(x,y,width,hight){
+    // ดึงกราบแคนวาสที่สร้างจาก index.html
+    let canvas = document.getElementById('canvas');
+    // สร้างสี่เหลี่ยมเก็บไว้ในตัวแปร context
+    let context = canvas.getContext('2d');
+    // สร้างแถวสามแถวจากตัวแปร j
+    for (let j=0; j<3; j++){
+        // สร้างแถวสามคอลัมภ์จากตัวแปร i
+        for (let i=0; i<3; i++){
+            // เติมสีรูปสี่เหลี่ยมจาการสุ่มสี
+            context.fillStyle = "#"+Randomcolours(6);
+            context.fillRect(x+i*(width+40), y+j*(hight+40), width, hight);
+            }
+        }
+        return context;
+    }
+// จุดเริ่มต้นแกน x.y
+squares(270,20,130,130)
