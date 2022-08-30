e8305bc718240e880cc2aec354bb0617efe6b58e
KN <n.khanthamalee@gmail.com>
2022-08-30 22:41:09 +0700

    something change

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ function App() {
         <h2><center>
           Name : Khanthamalee Na-udom
         </center></h2>
-        <a href="https://khanthamalee.github.io/9-random-colours-squares/">
+        <a href="https://khanthamalee.github.io/9-random-colours-squares-no-connect-API/">
           <button class="bubbly-button" >Random-colours</button>
         </a>
       </header> 
